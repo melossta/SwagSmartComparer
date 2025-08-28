ca0193f934898ae8c49b67fb26c99890bc27c98d
Melos <statovcimelos96@gmail.com>
2025-08-28 13:28:02 +0200

    Fix small bug CompareController

--- src/Storefront/Controller/CompareController.php
+++ src/Storefront/Controller/CompareController.php
@@ -2,19 +2,16 @@
 
 namespace SwagSmartComparer\Storefront\Controller;
 
-
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
-use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelAnalytics\SalesChannelAnalyticsCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-/**
- * @Route(defaults={"_routeScope"={"storefront"}})
- */
+#[Route(defaults: ['_routeScope' => ['storefront']])]
+
 class CompareController extends StorefrontController
 {
     private EntityRepository $productRepository;
@@ -24,42 +21,53 @@ class CompareController extends StorefrontController
         $this->productRepository = $productRepository;
     }
 
-    #[Route(path: '/compare', name: 'frontend.smc.compare.page',methods: ['GET'])]
-    public function comparePage(SessionInterface $session,SalesChannelContext $context):Response
+    /**
+     * Compare Page
+     */
+    #[Route(path: '/compare', name: 'frontend.smc.compare.page', methods: ['GET'])]
+    public function comparePage(SessionInterface $session, SalesChannelContext $context): Response
     {
         $ids = $session->get('smc_compare', []);
+
         $products = [];
         if (!empty($ids)) {
             $criteria = (new Criteria($ids))
                 ->addAssociation('manufacturer')
                 ->addAssociation('cover')
                 ->addAssociation('properties.group');
+
             $products = $this->productRepository->search($criteria, $context->getContext())->getEntities();
         }
+
         return $this->renderStorefront('@Storefront/storefront/page/compare/index.html.twig', [
             'products' => $products,
         ]);
     }
-    #[Route(path: '/compare/add/{productId}',name: 'frontend.smc.compare.add', methods: ['POST'])]
-    public function addProduct(string $productId,SessionInterface $session):Response
+
+    /**
+     * Add product to compare list
+     */
+    #[Route(path: '/compare/add/{productId}', name: 'frontend.smc.compare.add', methods: ['POST'])]
+    public function addProduct(string $productId, SessionInterface $session): Response
     {
         $ids = $session->get('smc_compare', []);
-        if(!in_array($productId, $ids, true)) {
+        if (!in_array($productId, $ids, true)) {
             $ids[] = $productId;
         }
         $session->set('smc_compare', $ids);
+
         return $this->redirectToRoute('frontend.smc.compare.page');
     }
-    #[Route(path: '/compare/remove/{productId}', name: 'frontend.smc.compare.remove', methods: ['POST'])]
 
-    public function removeProduct(string $productId,SessionInterface $session):Response
+    /**
+     * Remove product from compare list
+     */
+    #[Route(path: '/compare/remove/{productId}', name: 'frontend.smc.compare.remove', methods: ['POST'])]
+    public function removeProduct(string $productId, SessionInterface $session): Response
     {
-        $ids=array_filter($session->get('smc_compare', []));
+        $ids = array_filter($session->get('smc_compare', []), fn ($id) => $id !== $productId);
         $session->set('smc_compare', $ids);
 
         return $this->redirectToRoute('frontend.smc.compare.page');
     }
-
-
-
 }
